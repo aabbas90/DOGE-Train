b13fabe8ad399ebd2d4a5a8c62483587a5c14348
Ahmed Abbas <ahmed.a.mirza@hotmail.com>
2023-11-07 13:30:08 +0100

    fixed install script

--- install.sh
+++ install.sh
@@ -1,27 +1,25 @@
-#conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
-# conda install pytorch torchvision cudatoolkit=11.3 -c pytorch #to support sm_80 on A100.
 # Need python>=3.10
-# conda install pytorch==2.0.1 pytorch-cuda=11.8 -c pytorch -c nvidia
+
+conda install pytorch==2.0.1 pytorch-cuda=11.8 -c pytorch -c nvidia
 pip install pytorch-lightning==2.0.2
+
+# for GNN:
 pip install torch_geometric
 pip install torch-scatter -f https://data.pyg.org/whl/torch-2.0.1%2Bcu118.html
 
-# Only required to generate random problem instances:
-conda install -c conda-forge ecole
+conda install -c conda-forge ecole # required to generate random problem instances.
+pip install gurobipy # required for ground-truth generation.
 
-# pip install pyscipopt
-# pip install ecole
 pip install tqdm
-# conda install h5py
-
 pip install torchmetrics
-pip install gurobipy
-pip install tensorboardX
+pip install tensorboard
 
+# For config files:
 cd external/yacs
 python setup.py install
 cd ..
 
+# Parallel deferred min-marginal averaging algorithm:
 cd external/BDD
 python setup.py install
 cd ..
